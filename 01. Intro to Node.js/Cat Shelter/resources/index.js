df23e2e2cfc9d5217798706c82bd794062f059ac
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-09-14 15:13:22 +0300

    add rounting to addCats and addBreed

--- 01. Intro to Node.js/Cat Shelter/resources/index.js
+++ 01. Intro to Node.js/Cat Shelter/resources/index.js
@@ -1,28 +1,40 @@
 const http = require('http');
 const homeHtml = require('./views/home/index');
 const siteCss = require('./content/styles/site')
+const addBreadHtml = require('./views/addBreed')
+const addCatHtml = require('./views/addCat')
+
+const server = http.createServer(async (req, res) => {
 
 
-const server = http.createServer(async (req, res) =>{
-const url = req.url;
+    console.log(req.url);
 
-console.log('server is called');
+    if (req.url == '/') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.write(homeHtml);
 
-if(url == '/'){
-    res.writeHead(200, {
-    'Content-Type': 'text/html'
-})
-res.write(homeHtml);
+    } else if (req.url == '/content/styles/site.css') {
+        res.writeHead(200, {
+            'Content-Type': 'text/css'
+        })
+        res.write(siteCss);
 
-} else if(url == '/content/styles/site.css'){
-    res.writeHead(200, { 
-        'Content-Type': 'text/css'
-    })
-    res.write(siteCss);
+    } else if( req.url == '/cats/add-breed'){
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.write(addBreadHtml); 
 
-}
+    }else if( req.url == '/cats/add-cat'){
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.write(addCatHtml);
+    }
 
-res.end();
+    res.end();
 });
 
 server.listen(5000, () => console.log(`Server is running on port 5000...`));
