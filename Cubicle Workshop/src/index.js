9324299aa434ac4fbabb8536d623a37e5222efe0
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-09-22 18:32:06 +0300

    add homeController

--- Cubicle Workshop/src/index.js
+++ Cubicle Workshop/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig')
+const homeController = require('./controllers/homeController');
+
 
 const PORT = 5000;
 const app = express();
@@ -10,8 +12,6 @@ expressConfig(app);
 handlebarsConfig(app);
 
 
-app.get('/', (req, res)=>{
-res.render('index')
-});
+app.use(homeController);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
