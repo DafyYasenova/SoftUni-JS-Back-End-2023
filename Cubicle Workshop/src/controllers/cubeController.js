e5a63a6d9d1b6082d74f9a55a786d3321fa9a658
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-09-23 14:25:56 +0300

    add cube services

--- Cubicle Workshop/src/controllers/cubeController.js
+++ Cubicle Workshop/src/controllers/cubeController.js
@@ -1,13 +1,20 @@
 const router = require('express').Router();
 
+
+const cubeServices = require('../services/cubeServices');
+
 // Path = /cubes/create
-router.get('/create', (req, res)=>{
-    res.render('create')
-});
+router.get('/create', (req, res) => {
+    console.log(cubeServices.getAll());
+    res.render('create');
+}); 
 
-router.post('/create', (req, res)=>{
-   console.log(req.body);
+router.post('/create', (req, res) => {
+    const { name, description, imageUrl, difficultyLevel } = req.body;
 
-   res.redirect('/')
+    cubeServices.create({
+        name, description, imageUrl, difficultyLevel: Number(difficultyLevel)});
+
+    res.redirect('/')
 })
 module.exports = router;
