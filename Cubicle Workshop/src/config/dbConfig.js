8d2e7472389bea3a201404d4bb09c6f339313f85
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-09-28 11:27:30 +0300

    setup mongoose

---
+++ Cubicle Workshop/src/config/dbConfig.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const URI = 'mongodb://127.0.0.1:27017/cubicle'
+
+async function dbConnect(){
+    await mongoose.connect(URI)
+
+}
+
+module.exports = dbConnect;
--- Cubicle Workshop/src/index.js
+++ Cubicle Workshop/src/index.js
@@ -2,16 +2,20 @@ const express = require('express');
 
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig')
+const dbConnect = require('./config/dbConfig');
+
 const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
-
 
 const PORT = 5000;
 const app = express();
 
+dbConnect()
+.then(() => console.log('DB Connected successfully!'))
+.catch(err => console.log('DB error: ', err));
+
 expressConfig(app);
 handlebarsConfig(app);
-
 
 app.use(homeController);
 app.use('/cubes', cubeController);
