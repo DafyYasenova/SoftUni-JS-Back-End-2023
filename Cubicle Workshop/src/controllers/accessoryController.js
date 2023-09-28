e4decf49e91efafb66d53fb8d7df0ca090a20b55
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-09-28 19:06:40 +0300

    add post accessory

--- Cubicle Workshop/src/controllers/accessoryController.js
+++ Cubicle Workshop/src/controllers/accessoryController.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
+const accessoryServices = require('../services/accessoryServices');
 
 router.get('/create', (req, res) =>{
 res.render('accessory/create');
 });
 
-router.post('/create', (req, res) =>{
-    const body = req.body;
+router.post('/create', async (req, res) =>{
+    const { name, description, imageUrl} = req.body;
+await accessoryServices.create({ name, description, imageUrl});
 
 
     res.redirect('/');
