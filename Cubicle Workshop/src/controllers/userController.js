ad387af11eecb42331870ce99e0eb78a9c8b2d29
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-10-03 16:32:17 +0300

    add User model, controller ans services, and save user in Db with validate password

--- Cubicle Workshop/src/controllers/userController.js
+++ Cubicle Workshop/src/controllers/userController.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
 
+const userServices = require('../services/userServices');
+
 router.get('/register', (req, res) =>{
 res.render('users/register');
 });
 
+router.post('/register', async (req, res)=> {
+
+    const {username, password, repearPassword} = req.body;
+    
+    await userServices.register({username, password, repearPassword});
+
+    res.redirect('/users/login');
+});
  
 module.exports = router;
