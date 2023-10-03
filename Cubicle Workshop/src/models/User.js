321cedb44e8314f97c9e7edee1f7620641704e71
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-10-03 16:59:24 +0300

    hash password before save in DB, with bcrypt lib

--- Cubicle Workshop/src/models/User.js
+++ Cubicle Workshop/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: String,
@@ -9,9 +10,15 @@ const userSchema = new mongoose.Schema({
 
 userSchema.virtual('repeatPassword')
 .set(function(value){
-    if(value!== this.password){
+    if(value !== this.password){
         throw new mongoose.MongooseError('Password don\'t match!')
     }
+});
+
+userSchema.pre('save', async function(){
+const hash = await bcrypt.hash(this.password, 12);
+this.password = hash;
+
 })
 
 const User = mongoose.model('User', userSchema);
