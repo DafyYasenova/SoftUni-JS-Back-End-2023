818cd007876c3e3ccee9bb6835129313823d1280
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-10-05 07:41:02 +0300

    user login

--- Cubicle Workshop/src/controllers/userController.js
+++ Cubicle Workshop/src/controllers/userController.js
@@ -2,20 +2,29 @@ const router = require('express').Router();
 
 const userServices = require('../services/userServices');
 
-router.get('/register', (req, res) =>{
-res.render('users/register');
+router.get('/register', (req, res) => {
+    res.render('users/register');
 });
 
-router.post('/register', async (req, res)=> {
+router.post('/register', async (req, res) => {
 
-    const {username, password, repearPassword} = req.body;
-    
-    await userServices.register({username, password, repearPassword});
+    const { username, password, repearPassword } = req.body;
+
+    await userServices.register({ username, password, repearPassword });
 
     res.redirect('/users/login');
 });
 
 router.get('/login', (req, res) => {
     res.render('users/login');
+});
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await userServices.login(username, password);
+    console.log(user)
+    res.redirect('/');
+
 })
 module.exports = router;
