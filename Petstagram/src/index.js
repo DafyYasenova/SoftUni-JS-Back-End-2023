0b087e180ea97f61d6d3f116995baec5d24e71d5
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-10-13 15:42:18 +0300

    add homeController

--- Petstagram/src/index.js
+++ Petstagram/src/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
-const PORT = 3000;
-const app = express();
+const handlebars = require('express-handlebars');
+const path = require('path');
 
-app.get('/', (req, res) =>{
-    res.send('Action');
-});
+const PORT = 3000;
+
+const routes = require('./routes');
+
+
+const app = express();
+app.engine('hbs', handlebars.engine({extname: 'hbs'}));
+
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(routes);
+
 
 app.listen(PORT, console.log(`Server is listening on port ${PORT}...`));
