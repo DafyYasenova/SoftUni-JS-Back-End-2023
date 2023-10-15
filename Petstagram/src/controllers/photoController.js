0cfd6a5edcff62507289794984b6d8c3da47e6d4
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-10-15 21:10:10 +0300

    delete functionality

--- Petstagram/src/controllers/photoController.js
+++ Petstagram/src/controllers/photoController.js
@@ -2,25 +2,25 @@ const router = require('express').Router();
 
 const photoServices = require('../services/photoServices');
 
-const { extractErrorMessage} = require('../utils/errorHelpers');
+const { extractErrorMessage } = require('../utils/errorHelpers');
 
-router.get('/catalog', async (req, res) =>{
+router.get('/catalog', async (req, res) => {
 
     const photos = await photoServices.getAll().lean();
 
-    res.render('photos/catalog', {photos});
+    res.render('photos/catalog', { photos });
 })
 
 router.get('/create', (req, res) => {
 
     res.render('photos/create');
-   // res.redirect('/photos/catalog')
+    // res.redirect('/photos/catalog')
 
 });
 
 
 router.post('/create', async (req, res) => {
-    const photoData = {...req.body, owner: req.user._id};
+    const photoData = { ...req.body, owner: req.user._id };
 
     try {
 
@@ -28,17 +28,28 @@ router.post('/create', async (req, res) => {
         res.redirect('/photos/catalog');
 
     } catch (error) {
-        res.render('photos/create', { error: extractErrorMessage(error)});
+        res.render('photos/create', { error: extractErrorMessage(error) });
 
     }
 });
 
-router.get('/:photoId/details', async (req, res) =>{
+router.get('/:photoId/details', async (req, res) => {
     const photoId = req.params.photoId;
     const photo = await photoServices.getOne(photoId).lean();
 
     const isOwner = req.user?._id == photo.owner._id
-    res.render('photos/details', { photo , isOwner});
+    res.render('photos/details', { photo, isOwner });
 
+});
+
+router.get('/:photoId/delete', async (req, res) => {
+    const photoId = req.params.photoId;
+
+    try {
+        await photoServices.delete(photoId);
+        res.redirect('/photos/catalog')
+    } catch (error) {
+        res.render(`/photos/${photoId}/details`, {error: 'Unsuccessfull delete!'});
+    }
 })
 module.exports = router;
