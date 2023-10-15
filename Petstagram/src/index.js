832186f9c4ec84da52baace595b305b33d20d210
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-10-15 10:22:08 +0300

    add express and handlebars files for configurate

--- Petstagram/src/index.js
+++ Petstagram/src/index.js
@@ -1,34 +1,20 @@
 const { PORT }  = require('./config/constans');
 
 const express = require('express');
-const handlebars = require('express-handlebars');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-
-const dbConnect = require('./config/dbConfig')
-
-const { auth } = require('./middlewares/authMiddleware');
+const expressConfig = require('./config/expressConfig');
+const handlebarsConfig = require('./config/handlebarsConfig');
+const dbConnect = require('./config/dbConfig');
 const routes = require('./routes');
 
-
 const app = express();
+
+expressConfig(app);
+handlebarsConfig(app);
 
 dbConnect()
 .then(() => console.log('DB Connect successfully!'))
 .catch(err => console.log('DB Error:', err.message));
 
-
-app.engine('hbs', handlebars.engine({extname: 'hbs'}));
-
-app.set('view engine', 'hbs');
-app.set('views', 'src/views');
-
-
-app.use(express.static(path.resolve(__dirname, 'public')));
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(auth); // required after cookieParser!
 app.use(routes);
-
 
 app.listen(PORT, console.log(`Server is listening on port ${PORT}...`));
