068d4b0c47a9dd7f46927b8d480c34f5d867c12c
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-10-17 14:29:22 +0300

    add edit and delete functionality

--- CryptoTrade/src/controllers/cryptoController.js
+++ CryptoTrade/src/controllers/cryptoController.js
@@ -11,7 +11,7 @@ const { isAuth } = require('../middlewares/authMiddleware');
 router.get('/create', isAuth, (req, res) => {
 
     res.render('crypto/create');
-    
+
 });
 
 router.post('/create', isAuth, async (req, res) => {
@@ -32,7 +32,7 @@ router.get('/catalog', async (req, res) => {
 
     const crypto = await cryptoService.getAll().lean();
 
-    res.render('crypto/catalog', { crypto }); 
+    res.render('crypto/catalog', { crypto });
 });
 
 router.get('/:cryptoId/details', async (req, res) => {
@@ -42,7 +42,7 @@ router.get('/:cryptoId/details', async (req, res) => {
     const isOwner = req.user?._id == crypto.owner._id;
 
     //for buy:
-    const isBuyer = crypto.buyCrypto.some(id => id == req.user._id);
+    const isBuyer = crypto.buyCrypto?.some(id => id == req.user._id);
     res.render('crypto/details', { crypto, isOwner, isBuyer });
 
 });
@@ -62,10 +62,25 @@ router.get('/:cryptoId/edit', isAuth, async (req, res) => {
 
     const crypto = await cryptoService.getOne(req.params.cryptoId).lean();
 
-    res.render('crypto/edit', { photo });
+    // for selected paymentMethod:
+    const paymentMetodsMap = {
+        'crypto-wallet': 'Crypto Wallet',
+        'credit-card': 'Credit Card',
+        'debit-card': 'Debit Card',
+        'paypal': 'PayPal',
+    }
+
+    const paymentMethods = Object.keys(paymentMetodsMap).map(key => ({
+        value: key,
+        label: paymentMetodsMap[key],
+        isSelected: crypto.paymentMethod == key,
+    }));
+
+
+    res.render('crypto/edit', { crypto, paymentMethods });
 });
 
-router.post('/:crypto/edit', isAuth, async (req, res) => {
+router.post('/:cryptoId/edit', isAuth, async (req, res) => {
     const cryptoData = req.body;
     const cryptoId = req.params.cryptoId;
 
@@ -75,16 +90,17 @@ router.post('/:crypto/edit', isAuth, async (req, res) => {
         res.redirect(`/crypto/${cryptoId}/details`);
 
     } catch (error) {
+        
         res.render('crypto/edit', { error: 'Unable to update', ...cryptoData })
     }
 });
 
-router.get('/:cryptoId/buy', isAuth, async (req, res) =>{
+router.get('/:cryptoId/buy', isAuth, async (req, res) => {
     const cryptoId = req.params.cryptoId;
 
     await cryptoService.buy(req.user._id, req.params.cryptoId);
 
-     res.redirect(`/crypto/${cryptoId}/details`);
+    res.redirect(`/crypto/${cryptoId}/details`);
 })
 
 
