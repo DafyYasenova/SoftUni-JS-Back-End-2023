f0ec1e1a7f5b1c9e88442ee3d9e888dcf681609b
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-10-22 17:05:37 +0300

    create base models and controllers in server

--- REST API/server/index.js
+++ REST API/server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
+const routes = require('./routes')
 const app = express();
 
 app.get('/', (req, res) => {
 res.send('hello');
 
 });
+
+app.use('/data', routes);
 
 app.listen(3030, () => console.log('RESTful server is listening on port 3030...'));
---
+++ REST API/server/models/Furniture.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const furnitureSchema = new mongoose.Schema({
+
+
+});
+
+const Furniture = mongoose.model('Furniture', furnitureSchema);
+
+module.exports = Furniture;
