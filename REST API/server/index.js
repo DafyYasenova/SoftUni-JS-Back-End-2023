39b0486e4e1b2ce64ec246889ad2b0fef9a4a030
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-10-24 15:32:44 +0300

    setup mongodb and connect to db

--- REST API/server/index.js
+++ REST API/server/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 
 const cors = require('cors');
+const mongoose = require('mongoose');
 
-const routes = require('./routes')
+const routes = require('./routes');
 const app = express();
 
+
+mongoose.connect('mongodb://127.0.0.1:27017/furnitures')
+.then(() => console.log('DB Connected'))
+.catch(err => console.log(err));
+
 app.use(express.urlencoded({extended: false})); //for parse query strings
+app.use(cors());
 
 app.use(express.json()); //for parse json data
  
@@ -28,3 +35,4 @@ res.send('hello');
 app.use(routes);
 
 app.listen(3030, () => console.log('RESTful server is listening on port 3030...'));
+
